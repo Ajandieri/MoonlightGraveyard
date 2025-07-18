ab63c6eb3d43f9187ee5c3f08a27d78c2cc78075
Your Name <your.email@example.com>
2025-07-18 14:38:10 +0400

    Title and CV link

--- ResumeScrollData.js
+++ ResumeScrollData.js
@@ -12,7 +12,7 @@ export const RESUME_SCROLL_DATA = {
     url: 'https://i.postimg.cc/nCd56FB0/Resume-In-Game.png',
     // This is the direct download link for the file that will be downloaded
     // when the scroll is collected.
-    downloadUrl: 'https://drive.google.com/uc?export=download&id=1wCj817YF4erBGzoAg9tI2KbjlnmTUjHA',
+    downloadUrl: 'https://drive.google.com/uc?export=download&id=1X3FgPloBmi-Q8ib9bkCeZ5vvHQziwKNZ',
     // Time in milliseconds to wait after the scroll animation finishes before showing the dialogue.
     dialogueStartDelay: 2000,
     // Time in milliseconds to wait after the dialogue starts before triggering the download.
